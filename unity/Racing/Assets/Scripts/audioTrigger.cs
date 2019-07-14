f1728ee6908da7af532b60126846dbe030b8c670
Clint Brodar <clintbrodar@Clints-MacBook-Pro.local>
2019-07-13 21:04:54 -0700

    Lap time

---
+++ unity/Racing/Assets/Scripts/audioTrigger.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioTrigger : MonoBehaviour
+{
+    public GameObject countDown;
+    public AudioSource Soundtrack;
+    public GameObject lapTimer;
+    public GameObject carControls;
+    void Start()
+    {
+        StartCoroutine (countStart());
+    }
+    IEnumerator countStart () {
+        Soundtrack.Play();
+        yield return new WaitForSeconds (0.5f);
+        countDown.GetComponent<Text>().text = "5";
+        countDown.SetActive(true);
+        yield return new WaitForSeconds (1);
+        countDown.SetActive(false);
+        countDown.GetComponent<Text>().text = "4";
+        countDown.SetActive(true);
+        yield return new WaitForSeconds (1);
+        countDown.SetActive(false);
+        countDown.GetComponent<Text>().text = "3";
+        countDown.SetActive(true);
+        yield return new WaitForSeconds (1);
+        countDown.SetActive(false);
+        countDown.GetComponent<Text>().text = "2";
+        countDown.SetActive(true);
+        yield return new WaitForSeconds (1);
+        countDown.SetActive(false);
+        countDown.GetComponent<Text>().text = "1";
+        countDown.SetActive(true);
+        yield return new WaitForSeconds (1);
+        countDown.SetActive(false);
+        countDown.GetComponent<Text>().text = "Go!";
+        countDown.SetActive(true);
+        yield return new WaitForSeconds (1);
+        countDown.SetActive(false);
+
+        lapTimer.SetActive(true);
+        carControls.SetActive(true);
+    }
+
+}
---
+++ unity/Racing/Assets/Scripts/lapComplete.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lapComplete : MonoBehaviour
+{
+    public GameObject lapCompleteTrigger;
+    public GameObject halfPointTrigger;
+
+    public GameObject minuteDisplay;
+    public GameObject secondDisplay;
+    public GameObject milliDisplay;
+
+    public GameObject lapTimeBox;
+
+    void OnTriggerEnter () {
+        // refering to lapTimeManager script
+        if (lapTimeManager.secondCount <= 9) {
+            secondDisplay.GetComponent<Text>().text = "0" + lapTimeManager.secondCount + ".";
+        } else {
+            secondDisplay.GetComponent<Text>().text = "" + lapTimeManager.secondCount + ".";
+        }
+        if (lapTimeManager.minuteCount <= 9) {
+            minuteDisplay.GetComponent<Text>().text = "0" + lapTimeManager.minuteCount + ".";
+        } else {
+            minuteDisplay.GetComponent<Text>().text = "" + lapTimeManager.minuteCount + ".";
+        }
+        milliDisplay.GetComponent<Text>().text = "" + lapTimeManager.milliCount;
+
+        lapTimeManager.minuteCount = 0;
+        lapTimeManager.secondCount = 0;
+        lapTimeManager.milliCount = 0;
+        halfPointTrigger.SetActive(true);
+        lapCompleteTrigger.SetActive(false);
+    }
+}
