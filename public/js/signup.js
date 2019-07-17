4142d19487750f270fb7af43cb593bd06e7ce051
Surafel Ferede <surafelaron@gmail.com>
2019-07-16 19:28:13 -0700

    created some UI, user with first and last name

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,30 +1,39 @@
 $(document).ready(function() {
   // Getting references to our form and input
   var signUpForm = $("form.signup");
+  var firstNameInput= $("input#firstName-input");
+  var lastNameInput= $("input#lastName-input");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
-
+console.log(firstNameInput);
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
+      firstName: firstNameInput.val().trim(),
+      lastName: lastNameInput.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
-
+     console.log(userData);
     if (!userData.email || !userData.password) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.firstName, userData.lastName, userData.email, userData.password);
+    firstNameInput.val("");
+    lastNameInput.val("");
     emailInput.val("");
     passwordInput.val("");
+
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(firstName, lastName, email, password) {
     $.post("/api/signup", {
+      firstName: firstName,
+      lastName: lastName,
       email: email,
       password: password
     })
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,7 +14,10 @@ module.exports = function(app) {
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", function(req, res) {
+    console.log(req.body.firstName);
     db.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
       email: req.body.email,
       password: req.body.password
     })
