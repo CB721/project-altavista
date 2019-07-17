22cd6dfdf3f917e676928e3f4d85701c06d8c534
Surafel Ferede <surafelaron@gmail.com>
2019-07-16 19:28:13 -0700

    created some UI, user with first and last name

--- models/user.js
+++ models/user.js
@@ -5,6 +5,15 @@ var bcrypt = require("bcrypt-nodejs");
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
+      
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
