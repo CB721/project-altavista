545a8e203466b59e3897f2d61827a7d0a9ef6e7a
CB721 <clintbrodar@gmail.com>
2019-07-24 20:53:14 -0700

    First time play message, number of plays shown if user has played before

--- public/js/members.js
+++ public/js/members.js
@@ -3,7 +3,17 @@ $(document).ready(function() {
   // and updates the HTML on the page
   $.get("/api/users_profile").then(function(data) {
     $(".member-name").text(data.email);
-    $("#playerName").append(" " + data.firstName);
+    var timesPlayed = data.numberOfPlays;
+    // if this is their first time playing
+    if (timesPlayed === 0) {
+      // Special message if they haven't played
+      $("#playerName").append("Show Us What You Can Do " + data.firstName + "!");
+    } else {
+      // display their name
+      $("#playerName").append("Welcome back, " + data.firstName + "!");
+      // display how many times they have played
+      $("#timesPlayed").append("You have played " + timesPlayed + " times!");
+    }
   });
 
   // This function updates a todo in our database
