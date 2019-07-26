f6de937c2483206d696dc6a3569e416b554a13f1
CB721 <clintbrodar@gmail.com>
2019-07-26 07:50:27 -0700

    Update play total on play click, removed profile link - members page is profile, link from game back to profile

--- public/js/members.js
+++ public/js/members.js
@@ -1,28 +1,47 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/users_profile").then(function(data) {
-    $(".member-name").text(data.email);
+    var userID = data.id;
+    // set variable to be updated from db
     var timesPlayed = data.numberOfPlays;
+    console.log("Original plays: " + timesPlayed);
     // if this is their first time playing
     if (timesPlayed === 0) {
       // Special message if they haven't played
-      $("#playerName").append("Show Us What You Can Do " + data.firstName + "!");
+      $("#playerName").append(
+        "Show Us What You Can Do " + data.firstName + "!"
+      );
     } else {
       // display their name
       $("#playerName").append("Welcome back, " + data.firstName + "!");
       // display how many times they have played
       $("#timesPlayed").append("You have played " + timesPlayed + " times!");
     }
+    // when user clicks play button
+    $(".playCount").click(function() {
+      // add one to times played
+      var newPlays = timesPlayed + 1;
+      console.log("New play total: " + newPlays);
+      // send new total to update plays function
+      updatePlays(newPlays);
+    });
   });
-
-  // This function updates a todo in our database
-  // function updateTodo(todo) {
-  //   $.ajax({
-  //     method: "PUT",
-  //     url: "/api/todos",
-  //     data: todo
-  //   }).then(getTodos);
-  // }
+  // update play count in db
+  function updatePlays(numberOfPlays) {
+    $.ajax({
+      method: "PUT",
+      url: "/api/plays",
+      data: {
+        nop: numberOfPlays
+      }
+    }).done(function(response){
+      console.log("Response from on click ajax call: " + response);
+      // navigate to game page
+      // window.location.href = "localhost:8080/play";
+      // console.log(response);
+      // console.log(location);
+    });
+  }
+  // user delete account option
 });
-
